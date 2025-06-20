059a897d81486d3c2fca1c5a071c72e142055dbb
lamericaindotcom <pierre-ledez@hotmail.fr>
2025-06-20 15:14:28 +0200

    modify connexion.php

---
+++ app/View/Viewconnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta nanme="viewport" content="width=device-width, initial-scale-1.0">
+    <link rel="stylesheet" href="./public/assets/styles/main.css">
+    <script src="./public/assets/scripts/main.js"async></script>
+    <title>Connexion</title>
+</head>
+<body>
+     <h1>Se connecter</h1>
+     <form action="" mehod="post">
+          <label for="email">Veuillez saisir votre adresse email : </label>
+          <input type="email" name="email"
+          <label for="password">Veuillez saisir votre mot de passe : </label>
+	  <input type="password" name="password">
+	  <input type="submit" name:"submit" value="connexion">
+     </form>
+</body>
+</html>
